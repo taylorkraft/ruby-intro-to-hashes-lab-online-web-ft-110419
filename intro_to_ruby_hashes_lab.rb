a5b967fcd33069d2418aaebdb97aebb6c3b70fc8
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 15:12:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,10 @@ end
 def monopoly_with_second_tier
 	monopoly = {
 	  railroads: {
-	    pieces: "4"
+	    pieces: 4,
+	    names: {},
+	    rent_in_dollars: {},
+	    one_piece_owned: 25
 	  }
 	}
 
